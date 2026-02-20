9ab75701cf34f2a1a1515a5a8cdbe5cf4ceced90
Fominoshka <91738038+poliroika@users.noreply.github.com>
2026-02-20 13:14:14 +0300

    Delete rustworkx_framework directory

--- rustworkx_framework/config/__init__.py
+++
@@ -1,4 +0,0 @@
-from rustworkx_framework.config.logging import logger, setup_logging
-from rustworkx_framework.config.settings import FrameworkSettings, load_settings
-
-__all__ = ["FrameworkSettings", "load_settings", "logger", "setup_logging"]
